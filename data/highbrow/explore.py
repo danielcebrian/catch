5e1f9f826d4c3e362fe5e898b6c349186ba7c40a
Mark Soper <masoper@gmail.com>
2013-08-08 15:55:54 -0400

    data model ready for fixture loading

--- data/highbrow/explore.py
+++ data/highbrow/explore.py
@@ -9,6 +9,9 @@ context = data['@context']
 items = data['items']
 
 for item in items:
-	#print "item has keys: " + str(item.keys())
-	#print len(item["target"]["selector"])
-	print "annotatedBy: " + str(item["annotatedBy"])
+	print "item has keys: " + str(item.keys())
+	#print "selector array has length: " + str(len(item["target"]["selector"]))
+	#print "annotatedBy: " + str(item["annotatedBy"])
+	#print "selector keys" + str(item['target']['selector'][0].keys())
+	#print "target keys" + str(item['target'].keys())
+	#print "body keys" + str(item['body'].keys())
--- js/app.js
+++ js/app.js
@@ -26,30 +26,54 @@
   // MODELS
   //
 
-  App.Resource = DS.Model.extend({
+  App.Models = App.Models || {};
+
+  App.Models.Resource = DS.Model.extend({
     "@id": DS.attr('string'),
     "@type": DS.attr('string')
   });
 
-  App.Person = App.Resource.extend({
+  App.Models.Person = App.Models.Resource.extend({
     "name": DS.attr('string'),
     "foaf": DS.attr('string')  // ?? may not be needed - value may always be "Person"
   });
 
-  App.Target = App.Resource.extend({
-    "selector": DS.attr(array, "Selector")  // define this custom object type using registerTransform ??
+  App.Models.Selector = App.Models.Resource.extend({
+    "@type": DS.attr("string"),
+    "item": DS.attr("string"),  // serialized object
+    "0": DS.attr("string")  // serialized object
   });
 
-  App.Body = App.Resource.extend({
+  App.Models.Target = App.Models.Resource.extend({
+    "selector": DS.attr(array, "App.Models.Selector")
+  });
+
+  App.Models.Body = App.Models.Resource.extend({
     "chars": DS.attr('string')
   });
 
-  App.Annotation = App.Resource.extend({
+  App.Models.Annotation = App.Models.Resource.extend({
     "annotatedBy": DS.belongsTo("App.Person"),
     "annotatedAt": DS.attr('date'),
     "body": DS.belongsTo("App.Body"),
     "target": DS.belongsTo("App.Target")
   });
+
+  /*  TBD
+
+  App.MediaObject = App.Resource.extend({
+    "id": DS.attr('string'),
+    "title": DS.attr('string'),
+    "mediaType": DS.attr('string'),
+    "owner": DS.attr('string'),  // maybe App.Person ?
+    "client": DS.attr('string'),
+    "lastAnnotated": DS.attr('date'),  //
+    "annotationCount": DS.attr('integer'),
+    "numberOfTags": DS.attr('integer')
+  });
+
+  */
+
 
   //
   // DATA STORE
