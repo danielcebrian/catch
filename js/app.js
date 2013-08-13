3cb89f0536de2358e1ba05981cb5397f394919ff
Mark Soper <masoper@gmail.com>
2013-08-12 22:37:50 -0400

    recursive object render

--- js/app.js
+++ js/app.js
@@ -13,10 +13,37 @@
   //
 
   var compileTemplates = function() {
+    App.templates.annotationProperty = Handlebars.compile($("#annotation-property-template").text());
     App.templates.annotation = Handlebars.compile($("#annotation-template").text());
   };
 
 
+  //
+  // ANNOTATION RENDERING
+  //
+
+  var renderObject = function(obj, level) {
+    level = level || 0;
+    var html = "";
+    for (var prop in obj) {
+      var val = obj[prop];
+      if (typeof val === "object" && val.toString() === "[object Object]") {
+        html = html + App.templates.annotationProperty({
+          level: level,
+          property: prop,
+          value: renderObject(val, level + 1)
+        })
+      } else {
+        console.log("property: " + prop);
+        html = html + App.templates.annotationProperty({
+          level: level,
+          property: prop,
+          value: val
+        });
+      }
+    }
+    return html;
+  };
 
   //
   // START APP
@@ -24,11 +51,14 @@
 
   App.render = function() {
     App.dom.mainList = $("#main-list");
-    var mainHtml = "";
-    App.data.annotations.forEach(function(item) {
-      mainHtml = mainHtml + App.templates.annotation(item);
+    App.data.annotations.slice(0,10).forEach(function(item) {
+      var content = renderObject(item);
+      var annotationHtml = App.templates.annotation({
+        id: item["@id"],
+        content: content
+      });
+      App.dom.mainList.append(annotationHtml);
     });
-    App.dom.mainList.html(mainHtml);
   };
 
   //
@@ -38,10 +68,12 @@
   var loadFixtures = function() {
     var anno_json = $("#annotationsFixture").text();
     var anno_items = JSON.parse(anno_json)['items'];
+    /* optional
     anno_items.forEach(function(item) {
       item.id = item['@id'];
       item.type = item['@type'];
     });
+    */
     App.data.annotations = anno_items;
   };
 
